31f7a69cf58d20b3b0861d19817c5aabcf637f3d
Asher <ashermartin@gmail.com>
2022-11-02 23:11:47 -0700

    update first version!

---
+++ neuralnetwork.py
@@ -0,0 +1,11 @@
+import os
+import datetime
+
+import IPython
+import IPython.display
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import tensorflow as tf
